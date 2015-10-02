4c1073cee4c1a3e0470b4db193cb04931503a9e2
Max Maass <max@velcommuta.de>
2015-10-02 12:32:19 +0200

    Serialization and saving to disk, modulo sequence numbering

--- mobile/src/main/java/de/velcommuta/denul/service/PedometerService.java
+++ mobile/src/main/java/de/velcommuta/denul/service/PedometerService.java
@@ -13,6 +13,15 @@ import android.util.Log;
 
 import org.joda.time.DateTime;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.security.PublicKey;
 import java.util.Hashtable;
 
@@ -85,7 +94,7 @@ public class PedometerService extends Service implements SensorEventListener {
     @Override
     public void onDestroy() {
         mSensorManager.unregisterListener(this);
-        // TODO saveState();
+        saveState();
         Log.d(TAG, "onDestroy: Shutting down");
     }
 
@@ -110,6 +119,39 @@ public class PedometerService extends Service implements SensorEventListener {
     ///// Utility functions
 
     /**
+     * Save the state of the service into an encrypted file
+     */
+    private void saveState() {
+        // Serialize the state
+        byte[] state = serializeToByteArray(mHistory);
+        if (state == null) {
+            Log.e(TAG, "saveState: Something went wrong during serialization, aborting");
+            return;
+        }
+        // Encrypt the state
+        byte[] cipheredState = Crypto.encryptHybrid(state, mPubkey, 0); // TODO Sequence number
+        if (cipheredState == null) {
+            Log.e(TAG, "saveState: Something went wrong during state encryption, aborting");
+            return;
+        }
+        // Write to file, taking care not to overwrite existing state
+        int i = 0;
+        File file = new File(getFilesDir(), "pedometer.cache");
+        while (file.exists()) {
+            i += 1;
+            file = new File(getFilesDir(), "pedometer-" + i + ".cache");
+        }
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            fos.write(cipheredState);
+        } catch (IOException e) {
+            Log.e(TAG, "saveState: Encountered IOException during write, aborting.", e);
+            return;
+        }
+        Log.d(TAG, "saveState: Successfully wrote state to file");
+    }
+
+
+    /**
      * Load the public key from the shared Preferences
      * @return The PublicKey object from the shared preferences
      */
@@ -120,5 +162,44 @@ public class PedometerService extends Service implements SensorEventListener {
         if (pubkey == null) return null;
         // Decode the encoded pubkey into an actual pubkey object
         return Crypto.decodePublicKey(pubkey);
+    }
+
+    /**
+     * Serialize the hashtable into a byte[]
+     * @param ht The hashtable
+     * @return The serialized hashtable, or null if serialization failed
+     */
+    private byte[] serializeToByteArray(Hashtable ht) {
+        // Get ourselves set up for serialization
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+             ObjectOutput out = new ObjectOutputStream(bos)) {
+            // Pass the object into the serialization pipeline
+            out.writeObject(ht);
+            // Return the result
+            return bos.toByteArray();
+        } catch (IOException e) {
+            Log.e(TAG, "serializeToByteArray: An IOException occured, aborting");
+            return null;
+        }
+    }
+
+    /**
+     * Deserialize an object from a byte[]
+     * @param serialized The serialized object as a byte[]
+     * @return The deserialized Hashtable<DateTime, Long>, or null if an error occured
+     */
+    private Hashtable<DateTime, Long> deserializeFromByteArray(byte[] serialized) {
+        // Set up deserialization chain
+        try (ByteArrayInputStream bis = new ByteArrayInputStream(serialized);
+             ObjectInput in = new ObjectInputStream(bis)) {
+            // Deserialize the object and return it
+            return (Hashtable<DateTime, Long>) in.readObject();
+        } catch (IOException e) {
+            Log.e(TAG, "deserializeFromByteArray: Encountered IOException, aborting");
+            return null;
+        } catch (ClassNotFoundException e) {
+            Log.e(TAG, "deserializeFromByteArray: Encountered ClassNotFoundException, aborting");
+            return null;
+        }
     }
 }
