f9dfb2d293b52965d9a0c037b0407fa5686b8ba9
Max Maass <max@velcommuta.de>
2015-12-02 11:36:22 +0100

    Add animated fragment change

--- mobile/src/main/java/de/velcommuta/denul/ui/FriendAddActivity.java
+++ mobile/src/main/java/de/velcommuta/denul/ui/FriendAddActivity.java
@@ -2,6 +2,7 @@ package de.velcommuta.denul.ui;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -34,10 +35,40 @@ public class FriendAddActivity extends AppCompatActivity implements FriendAddTec
     }
 
 
+    /**
+     * Replace the currently active fragment with the Google Nearby fragment
+     */
+    private void slideInNearbyFragment() {
+        // Get new fragment instance
+        FriendAddTechSelectionFragment fr = FriendAddTechSelectionFragment.newInstance();
+        // Perform replacement
+        slideForwardReplace(fr);
+    }
+
+    /**
+     * Replace a fragment by sliding the old one out to the left, and the new one in from the right.
+     * Based on http://stackoverflow.com/a/4819665/1232833
+     * @param fragment The new fragment
+     */
+    private void slideForwardReplace(Fragment fragment) {
+        // Get a fragment transition
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        // Set up the animations to use
+        ft.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left);
+        // Perform the replacement
+        ft.replace(R.id.friend_add_container, fragment);
+        // Add to back stack
+        // TODO Implement proper backstack behaviour, it's broken at the moment
+        ft.addToBackStack(null);
+        // Commit transaction
+        ft.commit();
+    }
+
+
     @Override
     public void techSelection(int tech) {
         if (tech == FriendAddTechSelectionFragment.TECH_NEARBY) {
-            Toast.makeText(FriendAddActivity.this, "Nearby selected", Toast.LENGTH_SHORT).show();
+            slideInNearbyFragment();
         }
     }
 }
