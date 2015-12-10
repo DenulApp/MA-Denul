b14c58a14d7e42718f05ee4f693f2adf10b1b3c9
Max Maass <max@velcommuta.de>
2015-12-10 11:02:00 +0100

    Rename header => aad

--- mobile/src/main/java/de/velcommuta/denul/crypto/AES.java
+++ mobile/src/main/java/de/velcommuta/denul/crypto/AES.java
@@ -2,7 +2,6 @@ package de.velcommuta.denul.crypto;
 
 import android.util.Log;
 
-import java.security.AlgorithmParameters;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -70,11 +69,11 @@ public class AES {
      * Encrypt some data using AES in GCM
      * @param data The data to encrypt
      * @param keyenc The key, as byte[]
-     * @param header The header, to be added as associated data
+     * @param aad Additional authenticated data
      * @return The encrypted data as byte[], with the IV prepended to it
      */
-    public static byte[] encryptAES(byte[] data, byte[] keyenc, byte[] header) {
-        return encryptAES(data, keyenc, header, null);
+    public static byte[] encryptAES(byte[] data, byte[] keyenc, byte[] aad) {
+        return encryptAES(data, keyenc, aad, null);
     }
 
 
@@ -82,12 +81,12 @@ public class AES {
      * Encrypt some data using AES in GCM.
      * @param data The data that is to be encrypted
      * @param keyenc The key, as a byte[]
-     * @param header The header (with length field nulled), to be added as Associated Data
+     * @param aad The associated data to add (will not be included in the ciphertext, but authenticated)
      * @param iv The Initialization Vector to use. If it is null, a random IV will be generated
      * @return The encrypted data as a byte[]. If the IV was null, the used IV will be prepended
      *         to the message, otherwise the byte[] will not include the IV.
      */
-    public static byte[] encryptAES(byte[] data, byte[] keyenc, byte[] header, byte[] iv) {
+    public static byte[] encryptAES(byte[] data, byte[] keyenc, byte[] aad, byte[] iv) {
         try {
             boolean ivSpecified = iv != null;
             // Sanity check for IV length - we allow 16 byte (128 bit) and 32 byte (256 bit) IVs
@@ -108,8 +107,8 @@ public class AES {
                 iv = aesCipher.getIV();
             }
             // Add header to authentication
-            if (header != null) {
-                aesCipher.updateAAD(header);
+            if (aad != null) {
+                aesCipher.updateAAD(aad);
             }
             // Perform the encryption
             byte[] encrypted = aesCipher.doFinal(data);
@@ -153,17 +152,17 @@ public class AES {
      * Decrypt a piece of AES2656-encrypted data with its key
      * @param datawithiv Data with first bytes representing the IV
      * @param keyenc byte[]-encoded key
-     * @param header the header to be authenticated using AEAD
+     * @param aad Additional Authenticated Data to verify
      * @return Decrypted data as byte[]
      * @throws BadPaddingException If the padding was bad. This indicates that the ciphertext was
      * tampered with (i.e. the authentication failed)
      */
-    public static byte[] decryptAES(byte[] datawithiv, byte[] keyenc, byte[] header) throws BadPaddingException {
+    public static byte[] decryptAES(byte[] datawithiv, byte[] keyenc, byte[] aad) throws BadPaddingException {
         byte[] iv = new byte[16];
         byte[] encrypted = new byte[datawithiv.length -16];
         System.arraycopy(datawithiv, 0, iv, 0, 16);
         System.arraycopy(datawithiv, iv.length, encrypted, 0, datawithiv.length - 16);
-        return decryptAES(encrypted, keyenc, header, iv);
+        return decryptAES(encrypted, keyenc, aad, iv);
     }
 
 
@@ -171,13 +170,13 @@ public class AES {
      * Decrypt a piece of AES256-encrypted data with its key
      * @param encrypted Encrypted Data WITHOUT the IV
      * @param keyenc byte[]-encoded key
-     * @param header The header, to be authenticated using AEAD
+     * @param aad Additional authenticated data to verfiy
      * @param iv The initialization vector to use
      * @return Decrypted data as byte[]
      * @throws BadPaddingException If the padding was bad. This indicates that the ciphertext was
      * tampered with (i.e. the authentication failed)
      */
-    public static byte[] decryptAES(byte[] encrypted, byte[] keyenc, byte[] header, byte[] iv) throws BadPaddingException {
+    public static byte[] decryptAES(byte[] encrypted, byte[] keyenc, byte[] aad, byte[] iv) throws BadPaddingException {
         try {
             // Get Cipher instance
             Cipher aesCipher = Cipher.getInstance("AES/GCM/NoPadding", "SC");
@@ -186,8 +185,8 @@ public class AES {
             // Initialize cipher
             aesCipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
             // Add header for AAD
-            if (header != null) {
-                aesCipher.updateAAD(header);
+            if (aad != null) {
+                aesCipher.updateAAD(aad);
             }
             // Perform the decryption
             return aesCipher.doFinal(encrypted);
